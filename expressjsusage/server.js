3c53f89732d03486935a5a0fc65e83ad4219ee5b
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-12 11:41:07 +0100

    express.js testing

---
+++ expressjsusage/server.js
@@ -0,0 +1,21 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// get, post, put, delete methods
+app.get('/', (req, res) => {
+  res.send('<h1>Awesome website</h1>');
+})
+app.get('/data', (req, res) => {
+  // res.send('<h1>You again?</h1>');
+  dataObj = {
+    a: "a",
+    b: "b",
+    1: 1
+  }
+  res.send(dataObj);
+});
+app.listen(8080, () => {
+  console.log(`Don't call it a callback (listening on port ${port})`);
+});
