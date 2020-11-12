43451f3aed869652de118de3f341196501b31f39
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-12 15:05:35 +0100

    forgot second part

--- adventofcode2015/script.js
+++ adventofcode2015/script.js
@@ -4,10 +4,17 @@ const input = fs.readFileSync('./input.txt', 'utf-8');
 
 let floorUp = 0;
 let floorDown = 0;
+let firstTimeBasement = undefined;
 
 for(let i = 0; i < input.length; i++) {
   if (input[i] === '(') {floorUp++;}
   if (input[i] === ')') {floorDown--;}
+  if ((floorDown + floorUp) === -1) {
+    if (firstTimeBasement === undefined) {
+      firstTimeBasement = i + 1;
+    }
+  }
 }
 
 console.log(floorUp + floorDown);
+console.log(firstTimeBasement);
